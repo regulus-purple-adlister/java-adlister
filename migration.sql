885d010e426a6a5960428707272c74cdfeb4870e
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-02-14 16:40:00 -0600

    Updated migration script

--- migration.sql
+++ migration.sql
@@ -4,19 +4,28 @@ DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username VARCHAR(240) NOT NULL,
-    email VARCHAR(240) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       username VARCHAR(250) NOT NULL,
+                       email VARCHAR(250) NOT NULL,
+                       password VARCHAR(250) NOT NULL,
+                       PRIMARY KEY (id),
+                       UNIQUE (username)
 );
 
 CREATE TABLE ads (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    description TEXT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-        ON DELETE CASCADE
+                     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                     user_id INT UNSIGNED NOT NULL,
+                     title  VARCHAR(250) NOT NULL,
+                     description TEXT NOT NULL,
+                     PRIMARY KEY (id),
+                     FOREIGN KEY (user_id) REFERENCES users (id)
 );
+
+CREATE TABLE categories (
+                            id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                            name VARCHAR(250) NOT NULL,
+                            PRIMARY KEY (id)
+);
+
+-- TODO: create many to many table that links categories to ads
+
