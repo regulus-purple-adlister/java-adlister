0703a266f72ab5674defbafd8dee399468cedd67
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-02-14 16:18:07 -0600

    Completed migration script to create tables

---
+++ migration-script.sql
@@ -0,0 +1,22 @@
+USE adlister_db;
+
+DROP TABLE IF EXISTS ads;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+   username VARCHAR(250) NOT NULL,
+   email VARCHAR(250) NOT NULL,
+   password VARCHAR(250) NOT NULL,
+   PRIMARY KEY (id),
+   UNIQUE (username)
+);
+
+CREATE TABLE ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    title  VARCHAR(250) NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
