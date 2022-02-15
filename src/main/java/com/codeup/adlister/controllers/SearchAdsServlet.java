3ff96285090af9b55dc52a9ed237f4aa80b7568a
James Wienecke <james.a.wienecke@gmail.com>
2022-02-15 11:53:10 -0600

    Created a search servlet for showing a list of ads by user criteria.

---
+++ src/main/java/com/codeup/adlister/controllers/SearchAdsServlet.java
@@ -0,0 +1,24 @@
+package com.codeup.adlister.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "controllers.SeachAdsServlet", urlPatterns = "/search/")
+public class SearchAdsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        System.out.println();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String query = request.getParameter("search");
+        String type = request.getParameter("type");
+        System.out.println(query);
+        System.out.println(type);
+    }
+}
