0aa1a76bea9d93af11499d168420aa3e5e2b1c5c
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-02-15 13:34:55 -0600

    Completed MySQLCategoriesDao

--- src/main/java/com/codeup/adlister/dao/MySQLCategoriesDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLCategoriesDao.java
@@ -1,4 +1,80 @@
 package com.codeup.adlister.dao;
 
-public class MySQLCategoriesDao implements Cate {
+import com.codeup.adlister.models.Category;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLCategoriesDao implements Categories {
+    private Connection connection = null;
+
+    public MySQLCategoriesDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+    @Override
+    public List<Category> all() {
+        PreparedStatement stmt = null;
+        try {
+            stmt = connection.prepareStatement("SELECT * FROM categories");
+            ResultSet rs = stmt.executeQuery();
+            return createCategoriesFromResults(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving all ads.", e);
+        }
+    }
+
+    @Override
+    public Long insert(Category category) {
+        try {
+            String insertQuery = "INSERT INTO categories(id, name) VALUES (?, ?)";
+            PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+            stmt.setLong(1, category.getId());
+            stmt.setString(2, category.getName());
+
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new category.", e);
+        }
+    }
+
+    private Category extractCategory(ResultSet rs) throws SQLException {
+        return new Category(
+                rs.getLong("id"),
+                rs.getString("name")
+        );
+    }
+
+    private List<Category> createCategoriesFromResults(ResultSet rs) throws SQLException {
+        List<Category> categories = new ArrayList<>();
+        while (rs.next()) {
+            categories.add(extractCategory(rs));
+        }
+        return categories;
+    }
+
+    public Category findByCategory(String category) {
+        String query = "SELECT * FROM categories WHERE category = ?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, category);
+            return extractCategory(stmt.executeQuery());
+        } catch (SQLException e) {
+            throw new RuntimeException("Error finding category", e);
+        }
+    }
 }
